fe941802702997e511737e0da69ab85f75ad7dd2
Yrvine <yrvine@google.com>
2021-11-30 21:31:22 +0000

    intial commit

---
+++ pipeline/pubsub/README.md
@@ -0,0 +1,19 @@
+# Cloud Run Pub/Sub Tutorial Sample
+
+This sample shows how to create a service that processes Pub/Sub messages.
+
+Use it with the [Cloud Pub/Sub with Cloud Run tutorial](http://cloud.google.com/run/docs/tutorials/pubsub).
+
+For more details on how to work with this sample read the [Google Cloud Run Java Samples README](https://github.com/GoogleCloudPlatform/java-docs-samples/tree/main/run).
+
+[![Run in Google Cloud][run_img]][run_link]
+
+[run_img]: https://storage.googleapis.com/cloudrun/button.svg
+[run_link]: https://deploy.cloud.run/?git_repo=https://github.com/GoogleCloudPlatform/java-docs-samples&dir=run/pubsub
+
+
+## Dependencies
+
+* **Spring Boot**: Web server framework.
+* **Jib**: Container build tool.
+* **Junit**: [development] Test running framework.
---
+++ pipeline/pubsub/src/main/java/com/example/cloudrun/LoadAvroFromGcs.java
@@ -0,0 +1,47 @@
+package com.example.cloudrun;
+
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryException;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.FormatOptions;
+import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.JobInfo;
+import com.google.cloud.bigquery.LoadJobConfiguration;
+import com.google.cloud.bigquery.TableId;
+
+// Sample to load Avro data from Cloud Storage into a new BigQuery table
+public class LoadAvroFromGcs {
+
+//   public static void main(String[] args) {
+//     String datasetName = "MY_DATASET_NAME";
+//     String tableName = "MY_TABLE_NAME";
+//     String sourceUri = "gs://cloud-samples-data/bigquery/us-states/us-states.avro";
+//     sts
+//   }
+
+  public static void loadAvroFromGcs(String datasetName, String tableName, String sourceUri) {
+    try {
+      // Initialize client that will be used to send requests. This client only needs to be created
+      // once, and can be reused for multiple requests.
+      BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+
+      TableId tableId = TableId.of(datasetName, tableName);
+      LoadJobConfiguration loadConfig =
+          LoadJobConfiguration.of(tableId, sourceUri, FormatOptions.avro());
+
+      // Load data from a GCS Avro file into the table
+      Job job = bigquery.create(JobInfo.of(loadConfig));
+      // Blocks until this load table job completes its execution, either failing or succeeding.
+      job = job.waitFor();
+      if (job.isDone()) {
+        System.out.println("Avro from GCS successfully loaded in a table");
+      } else {
+        System.out.println(
+            "BigQuery was unable to load into the table due to an error:"
+                + job.getStatus().getError());
+      }
+    } catch (BigQueryException | InterruptedException e) {
+      System.out.println("Column not added during load append \n" + e.toString());
+    }
+  }
+}
